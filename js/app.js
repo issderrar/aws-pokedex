4f5fcf70f7290790e32a4d1a211e9c7471120569
Issam <issam_derrar@yahoo.fr>
2020-10-07 19:00:39 +0200

    it's working

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
 const myTitle = document.querySelector('#card-title');
-const myTypes = document.querySelector('.card-types');
+const myType = document.querySelector('#card-type');
 const myText = document.querySelector('#card-text');
+const myImage = document.querySelector('#card-img');
 
-var myInit = { method: 'GET',
-    headers: 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-    mode: 'cors',
-    cache: 'default' };
 
-fetch('https://8opdbbyonh.execute-api.eu-west-3.amazonaws.com/test/helloworld?name=Issam&city=Seattle', myInit)
-    .then(function(response) {
-        return response.blob();
-    })
-    .then(function(myBlob) {
-        const objectURL = URL.createObjectURL(myBlob);
-        console.log(objectURL)
-    });
+fetch("https://8opdbbyonh.execute-api.eu-west-3.amazonaws.com/test/helloworld")
+    .then(response => response.json())
+    .then(response => {
+            myText.textContent = "Voici votre Pokémon"
+            myTitle.textContent = response.name
+            myType.textContent = response.type
+            if (response.type === 'Electrique') {
+                myType.classList.add('btn-warning')
+                myImage.setAttribute("src", 'https://www.pokepedia.fr/images/0/0f/Miniature_025_RS.gif')
+
+            } else if (response.type === 'Eau') {
+                myType.classList.add('btn-primary')
+                myImage.setAttribute("src", 'https://www.pokepedia.fr/images/4/46/Miniature_007_RS.gif')
+
+
+            } else {
+                myType.classList.add('btn-danger')
+                myImage.setAttribute("src", 'https://www.pokepedia.fr/images/7/78/Miniature_006_RS.gif')
+            }
+        }
+    )
+    .catch(error => alert("Erreur : " + error));
