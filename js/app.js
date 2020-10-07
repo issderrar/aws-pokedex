04b6a64084f70b0679fb7d584523e869ecaba494
Issam <issam_derrar@yahoo.fr>
2020-10-07 18:39:29 +0200

    added fetch url

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
-const myTitle = document.querySelector('card-title');
+const myTitle = document.querySelector('#card-title');
+const myTypes = document.querySelector('.card-types');
+const myText = document.querySelector('#card-text');
 
-fetch('https://8opdbbyonh.execute-api.eu-west-3.amazonaws.com/test/helloworld?name=Issam&city=Seattle')
+var myInit = { method: 'GET',
+    headers: 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+    mode: 'cors',
+    cache: 'default' };
+
+fetch('https://8opdbbyonh.execute-api.eu-west-3.amazonaws.com/test/helloworld?name=Issam&city=Seattle', myInit)
     .then(function(response) {
         return response.blob();
     })
